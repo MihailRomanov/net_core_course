8746a1ba4aeb1ebd52ea6bba8b0cf8b6332edad1
Mihail Romanov <romanov.m@skbkontur.ru>
2024-08-30 15:06:08 +0400

    Update module 05 lectures

---
+++ Modules/04. Basic BCL/Samples/BookStoreLibrary_v2/BookStoreLibrary.Implementation/RepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Abstractions;
+
+namespace BookStoreLibrary.Implementation
+{
+    public abstract class RepositoryBase<TEntity> where TEntity : class
+    {
+        private readonly IFileSystem fileSystem;
+        private string bookStoreFileName;
+        protected string delim = "|||";
+
+        public RepositoryBase(string bookStoreFileName, IFileSystem fileSystem)
+        {
+            this.bookStoreFileName = bookStoreFileName;
+            this.fileSystem = fileSystem;
+        }
+
+
+        protected IEnumerable<TEntity> Load()
+        {
+            foreach (var line in fileSystem.File.ReadAllLines(bookStoreFileName))
+                yield return ToEntity(line);
+        }
+
+        protected abstract TEntity ToEntity(string line);
+
+    }
+}
