1fc018d07218f6353bc62ed274622b24a1535191
Mihail Romanov <romanov.m@skbkontur.ru>
2024-12-08 18:44:11 +0400

    Add materials

---
+++ Modules/11. Build, deploy and CI_CD/Samples/PackageSample/PackageSample/HelperClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PackageSample
+{
+    public static class JsonHelper
+    {
+        public static string ToJson(object obj)
+        {
+            if (obj is string)
+                return $"\"{obj}\"";
+
+            throw new NotSupportedException("Type is not supported!!!");
+        }
+    }
+}
